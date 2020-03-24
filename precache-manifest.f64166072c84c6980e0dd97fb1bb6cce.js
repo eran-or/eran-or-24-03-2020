ce7ed19b5ceeb81bfc36fca1c4f65d4d1326f827
System Administrator <root@Erans-MacBook-Pro.local>
2020-03-24 20:06:40 +0200

    Updates

---
+++ precache-manifest.f64166072c84c6980e0dd97fb1bb6cce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d5c08d4bf63873daaa886e9ad9547c",
+    "url": "/eran-or-24-03-2020/index.html"
+  },
+  {
+    "revision": "68c432e06bc0df026005",
+    "url": "/eran-or-24-03-2020/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b5d5d739ea92f1f4ef75",
+    "url": "/eran-or-24-03-2020/static/css/main.52f1e3e8.chunk.css"
+  },
+  {
+    "revision": "68c432e06bc0df026005",
+    "url": "/eran-or-24-03-2020/static/js/2.5ed871d6.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/eran-or-24-03-2020/static/js/2.5ed871d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d5d739ea92f1f4ef75",
+    "url": "/eran-or-24-03-2020/static/js/main.51a04cb3.chunk.js"
+  },
+  {
+    "revision": "43bfc3f0e2829a0a54ef",
+    "url": "/eran-or-24-03-2020/static/js/runtime-main.c34be7bf.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/eran-or-24-03-2020/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/eran-or-24-03-2020/static/media/16-s.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/eran-or-24-03-2020/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/eran-or-24-03-2020/static/media/41-s.421c13f1.png"
+  },
+  {
+    "revision": "f2f35d93818ab5d327a89d6b2ab1052c",
+    "url": "/eran-or-24-03-2020/static/media/fav.f2f35d93.svg"
+  },
+  {
+    "revision": "94a6f9b89ffe7d5bd83dbffce66b3ffa",
+    "url": "/eran-or-24-03-2020/static/media/search.94a6f9b8.svg"
+  },
+  {
+    "revision": "183aeb05d6b760a5aa70dc7ef79fafbf",
+    "url": "/eran-or-24-03-2020/static/media/unfav.183aeb05.svg"
+  }
+]);
